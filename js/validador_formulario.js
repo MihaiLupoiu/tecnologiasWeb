e05dde790056b6515c14a6a23616a41efeaa5b5a
Mihai Alexandru Lupoiu <myhay.360@gmail.com>
2014-10-14 20:26:59 +0200

    Versión casi acabada, pero no inserta en base de datos... y por consola si que funciona... Pendiente de revisar con la profesora

---
+++ js/validador_formulario.js
@@ -0,0 +1,48 @@
+document.write("<script type='text/javascript' src='./../js/web_db_local.js'></script>");
+
+function validacion() {
+    //crear lista vacía para añadir los valores
+    var lista_sql = ["SELECT", "DELETE", "FROM", "WHERE", "JOIN", "GROUP BY","*", "=", "%", "LIKE"];
+    var inputs = document.getElementsByTagName('input');
+    var date_rightnow = new Date();
+    for(var i = 0; i < inputs.length; i++) {
+        
+        var valor = inputs[i].value.toUpperCase(); 
+        if (lista_sql.indexOf(valor) > -1 ){
+            alert('[ERROR] El campo no puede tener el valor '+inputs[i].value);
+            return false;
+        }
+        //inputs["fecha_ini"] me devuelve el valor
+        else if (inputs[i].getAttribute('id') =='fecha_ini'){
+            var startdate = new Date(inputs[i].value);
+            //alert(startdate);
+            if (startdate < date_rightnow ){
+                alert('[ERROR] Fecha inicio evento no puede ser menor que la fecha actual '+inputs[i].value);
+                return false;
+            }
+        }
+        else if (inputs[i].getAttribute('id') =='fecha_fin'){
+            var enddate =  new Date(inputs[i].value);
+            //alert(enddate);
+            if (enddate < startdate){
+                alert('[ERROR] Fecha finalización evento no puede ser menor que la de inicio '+inputs[i].value);
+                return false;
+            }
+        }
+        else if (inputs[i].getAttribute('id') =='precio'){
+            if (inputs[i].value < 0){
+                alert('[ERROR] Precio de evento no puede ser'+inputs[i].value);
+                return false;
+            }
+        }
+    }
+    lista_eventos = crearlista(inputs);
+    addEvent(lista_eventos);
+}
+
+function crearlista(lista_imputs){
+    var variables = document.getElementsByTagName('select');
+    var descipcion = document.getElementsByTagName('textarea');
+    var lista_valores_evento = [lista_imputs[0].value,lista_imputs[1].value,lista_imputs[2].value,lista_imputs[3].value,variables[0].value,lista_imputs[4].value,variables[1].value,lista_imputs[5].value,variables[2].value,descipcion[0].value,lista_imputs[6].value,lista_imputs[7].value]
+    return lista_valores_evento;
+}
--- js/web_db_local.js
+++ js/web_db_local.js
@@ -1,12 +1,12 @@
 //Iniciación de BD
-var html5rocks = {};
-html5rocks.webdb = {};
-html5rocks.webdb.db = null;
+var develup = {};
+develup.webdb = {};
+develup.webdb.db = null;
 
 //Se crea la base de datos denominada "Todo" con un tamaño de 5Mb
-html5rocks.webdb.open = function() {
+develup.webdb.open = function() {
   var dbSize = 5 * 1024 * 1024; // 5MB
-  html5rocks.webdb.db = openDatabase("DevelUP", "1.0", "Base de datos de DevelUP", dbSize);
+  develup.webdb.db = openDatabase("DevelUP", "1.0", "Base de datos de DevelUP", dbSize);
                                     //Nombre, Versión, Descripción, Tamaño
 }
 
@@ -14,84 +14,60 @@ html5rocks.webdb.open = function() {
 //Donde ID es un numero autoincrementado y clave primaria
 //Todo es un campo tipo texto
 //Added_on es un campo de fecha
-html5rocks.webdb.createTable = function() {
-  var db = html5rocks.webdb.db;
+develup.webdb.createTable = function() {
+  var db = develup.webdb.db;
   db.transaction(function(tx) {
     tx.executeSql("CREATE TABLE IF NOT EXISTS eventos(ID INTEGER PRIMARY KEY ASC, nombre TEXT, fecha_ini DATETIME, fecha_fin DATETIME, precio INTEGER, publico TEXT, asistentes INTEGER, lugar TEXT, recinto TEXT, tipo TEXT, descripcion TEXT, imagen TEXT, mail TEXT)", []);
   });
 }
 
 //Inserta en la base de datos un nombre todo y se le asigna una fecha
-html5rocks.webdb.add_evento = function(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) {
-  var db = html5rocks.webdb.db;
-  db.transaction(function(tx){
+develup.webdb.add_evento = function(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) {
+    var db = develup.webdb.db;
+    //alert(nombre+" "+fecha_ini+" "+fecha_fin+" "+precio+" "+publico+" "+asistentes+" "+lugar+" "+recinto+" "+tipo);
+    db.transaction(function(tx){
     tx.executeSql("INSERT INTO eventos(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
-        [nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail],
-        html5rocks.webdb.onSuccess,
-        html5rocks.webdb.onError);
+        [nombre, fecha_ini, fecha_fin, parseInt(precio), publico, parseInt(asistentes), lugar, recinto, tipo, descripcion, imagen, mail],
+        develup.webdb.onSuccess,
+        develup.webdb.onError);
    });
 }
 
+//Inserta en la base de datos un nombre todo y se le asigna una fecha
+develup.webdb.add_evento2 = function(vector) {
+    var db = develup.webdb.db;
+    //alert(nombre+" "+fecha_ini+" "+fecha_fin+" "+precio+" "+publico+" "+asistentes+" "+lugar+" "+recinto+" "+tipo);
+    db.transaction(function(tx){
+    tx.executeSql("INSERT INTO eventos(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
+        vector,
+        develup.webdb.onSuccess,
+        develup.webdb.onError);
+   });
+}
+
+
 //Muestra por pantalla si ha habido un error
-html5rocks.webdb.onError = function(tx, e) {
-  alert("There has been an error: " + e.message);
+develup.webdb.onError = function(tx, e) {
+    console.log(e.message);
+    alert("There has been an error: " + e.message);
 }
 
 //Muestra todo los elementos de la base de datos
-html5rocks.webdb.onSuccess = function(tx, r) {
+develup.webdb.onSuccess = function(tx, r) {
   // re-render the data.
-  //html5rocks.webdb.getAllTodoItems(loadTodoItems);
+    alert("Datos añadidos de forma satisfactoria.");
     console.log("exit on success");
-}
-
-//Muestra todos los datos de una variable
-html5rocks.webdb.getAllTodoItems = function(renderFunc) {
-  var db = html5rocks.webdb.db;
-  db.transaction(function(tx) {
-    tx.executeSql("SELECT * FROM todo", [], renderFunc,
-        html5rocks.webdb.onError);
-  });
-}
-
-//Elimina todos los datos donde el id coincide
-html5rocks.webdb.deleteTodo = function(id) {
-  var db = html5rocks.webdb.db;
-  db.transaction(function(tx){
-    tx.executeSql("DELETE FROM todo WHERE ID=?", [id],
-        html5rocks.webdb.onSuccess,
-        html5rocks.webdb.onError);
-    });
-}
-
-//Imprime todos los datos
-function loadTodoItems(tx, rs) {
-  var rowOutput = "";
-  var todoItems = document.getElementById("todoItems");
-  for (var i=0; i < rs.rows.length; i++) {
-    rowOutput += renderTodo(rs.rows.item(i));
-  }
-
-  todoItems.innerHTML = rowOutput;
-}
-
-//Imprime en forma de lista todo los datos
-function renderTodo(row) {
-  return "<li>" + row.todo  + " [<a href='javascript:void(0);'  onclick='html5rocks.webdb.deleteTodo(" + row.ID +");'>Delete</a>]</li>";
 }
 
 //orden de ejecución de las funciones
 function init() {
-  html5rocks.webdb.open();
-  html5rocks.webdb.createTable();
-  //html5rocks.webdb.getAllTodoItems(loadTodoItems);
+  develup.webdb.open();
+  develup.webdb.createTable();
 }
 
-function addEvent() {
-  var lista_datos_evento = document.getElementById("form_evento").elements;
-    html5rocks.webdb.add_evento(lista_datos_evento[0].value, lista_datos_evento[1].value, lista_datos_evento[2].value, lista_datos_evento[3].value, lista_datos_evento[4].value, lista_datos_evento[5].value, lista_datos_evento[6].value, lista_datos_evento[7].value, lista_datos_evento[8].value, lista_datos_evento[9].value, lista_datos_evento[10].value, lista_datos_evento[11].value)
-    
-    
-    //for(var i = 0; i< lista_datos_evento.length; i++ ){
-        //html5rocks.webdb.add_event(lista_datos_evento[i].value);
-    //}
+function addEvent(lista_datos_evento) {
+    //alert('Dentro de db '+lista_datos_evento);
+    console.log(lista_datos_evento);
+    develup.webdb.add_evento2(lista_datos_evento);
+    //develup.webdb.add_evento(lista_datos_evento[0], lista_datos_evento[1], lista_datos_evento[2], lista_datos_evento[3], lista_datos_evento[4], lista_datos_evento[5], lista_datos_evento[6], lista_datos_evento[7], lista_datos_evento[8], lista_datos_evento[9], lista_datos_evento[10], lista_datos_evento[11]);
 }
