32836ba453842083a702bd0ed977f054a29abcee
Mihaita Alexandru Lupoiu <myhay.360@gmail.com>
2014-10-23 21:57:11 +0200

    Mejoras Listar y Objeto de añadir en base de datos creado

--- js/web_db_local.js
+++ js/web_db_local.js
@@ -5,19 +5,34 @@ develup.webdb.db = null;
 
 //objeto evento
 
-var evento = new Object();
-evento.nombre = "nombre";
-evento.fecha_ini = "22-02-2014"
-evento.fecha_fin = "31-03-2014";
-evento.precio = "precio del evento";
-evento.publico = 500;
-evento.asistentes = "Mixto";
-evento.lugar = "Castellon";
-evento.recinto = "Ribalta";
-evento.tipo = "LanParty";
-evento.descripcion = "asdf";
-evento.imagen = "";
-evento.mail = "22@gmail.com";
+var Objetoevento = new Object();
+Objetoevento.nombre = "nombre";
+Objetoevento.fecha_ini = "22-02-2014";
+Objetoevento.fecha_fin = "31-03-2014";
+Objetoevento.precio = "precio del evento";
+Objetoevento.publico = "500";
+Objetoevento.asistentes = "Mixto";
+Objetoevento.lugar = "Castellon";
+Objetoevento.recinto = "Ribalta";
+Objetoevento.tipo = "LanParty";
+Objetoevento.descripcion = "asdf";
+Objetoevento.imagen = "";
+Objetoevento.mail = "22@gmail.com";
+
+
+Objetoevento.constructorEvento = function(nombre,fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail)
+{this.nombre = nombre;
+this.fecha_ini = fecha_ini;
+this.fecha_fin = fecha_fin;
+this.precio = precio;
+this.publico = publico;
+this.asistentes = asistentes;
+this.lugar = lugar;
+this.recinto = recinto;
+this.tipo = tipo;
+this.descripcion = descripcion;
+this.imagen = imagen;
+this.mail = mail;}
 
 
 //Se crea la base de datos denominada "Todo" con un tamaño de 5Mb
@@ -39,12 +54,11 @@ develup.webdb.createTable = function() {
 }
 
 //Inserta en la base de datos un nombre todo y se le asigna una fecha
-develup.webdb.add_evento = function(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) {
+develup.webdb.add_evento = function(objeto_evento) {
     var db = develup.webdb.db;
-    //alert(nombre+" "+fecha_ini+" "+fecha_fin+" "+precio+" "+publico+" "+asistentes+" "+lugar+" "+recinto+" "+tipo);
     db.transaction(function(tx){
     tx.executeSql("INSERT INTO eventos(nombre, fecha_ini, fecha_fin, precio, publico, asistentes, lugar, recinto, tipo, descripcion, imagen, mail) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)",
-        [nombre, fecha_ini, fecha_fin, parseInt(precio), publico, parseInt(asistentes), lugar, recinto, tipo, descripcion, imagen, mail],
+        [objeto_evento.nombre, objeto_evento.fecha_ini, objeto_evento.fecha_fin, parseInt(objeto_evento.precio), objeto_evento.publico, parseInt(objeto_evento.asistentes), objeto_evento.lugar, objeto_evento.recinto, objeto_evento.tipo, objeto_evento.descripcion, objeto_evento.imagen, objeto_evento.mail],
         develup.webdb.onSuccess,
         develup.webdb.onError);
    });
@@ -74,6 +88,8 @@ develup.webdb.getAllTodoItems = function(renderFunc) {
       
 function loadTodoItems(tx, rs) {
         var rowOutput = "";
+        texto = "<tr> <td> nombre </td><td> fecha inicio </td><td>fecha fin</td><td>precio</td><td>publico</td><td>asistentes</td><td>lugar</td><td>recinto</td><td>tipo</td><td>descripcion</td><td> imagen </td><td>mail</td></tr>"
+        rowOutput += texto;
         var todoItems = document.getElementById("todoItems");
         for (var i=0; i < rs.rows.length; i++) {
           rowOutput += renderTodo(rs.rows.item(i));
@@ -83,7 +99,9 @@ function loadTodoItems(tx, rs) {
       }
 
 function renderTodo(row) {
-        return "<li>" + row.nombre  + " [<a href='javascript:void(0);'  onclick='develup.webdb.deleteTodo(" + row.ID +");'>Delete</a>]</li>";
+        return "<tr>" + "<td>" + row.nombre +  "</td>" + "<td>" + row.fecha_ini +  "</td>" + "<td>" + row.fecha_fin +  "</td>" + "<td>" + row.precio +  "</td>" +
+        "<td>" + row.publico +  "</td>" + "<td>" + row.asistentes + "</td>" + "<td>" + row.lugar +  "</td>" + "<td>" + row.recinto +  "</td>" + "<td>" + row.tipo +  "</td>" +
+        "<td>" + row.descripcion + "</td>" + "<td>" + row.imagen + "</td>" + "<td>" + row.mail +  "</td>" + "<td>[<a href='javascript:void(0);'  onclick='develup.webdb.deleteTodo(" + row.ID +");'>Delete</a>]</td></tr>";
       }
 
 develup.webdb.deleteTodo = function(id) {
@@ -108,5 +126,6 @@ function init() {
 function addEvent(lista_datos_evento) {
     //alert('Dentro de db '+lista_datos_evento);
     console.log(lista_datos_evento);
-    develup.webdb.add_evento(lista_datos_evento[0], lista_datos_evento[1], lista_datos_evento[2], lista_datos_evento[3], lista_datos_evento[4], lista_datos_evento[5], lista_datos_evento[6], lista_datos_evento[7], lista_datos_evento[8], lista_datos_evento[9], lista_datos_evento[10], lista_datos_evento[11]);
+    evento = new Objetoevento.constructorEvento(lista_datos_evento[0], lista_datos_evento[1], lista_datos_evento[2], lista_datos_evento[3], lista_datos_evento[4], lista_datos_evento[5], lista_datos_evento[6], lista_datos_evento[7], lista_datos_evento[8], lista_datos_evento[9], lista_datos_evento[10], lista_datos_evento[11]);
+    develup.webdb.add_evento(evento);
 }
